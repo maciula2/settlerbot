7bf532f74bb816bce8b039088d332d298b618525
= <=>
2025-12-05 00:55:49 +0100

    Add requirements.txt with initial dependencies for the project

---
+++ README.md
@@ -0,0 +1,22 @@
+# SettlerBot
+
+Tool for automating various actions in The Settlers Online.
+
+
+## Getting Started
+
+### Prerequisites
+- Python 3.13 (OpenCV required for image location doesn't work on 3.14 yet)
+### Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/maciula2/settlerbot.git
+   ```
+2. Navigate to the project directory:
+    ```bash
+    cd settlerbot
+    ```
+3. Install python dependencies:
+   ```bash
+   pip install -r requirements.txt
+   ```
--- main.py
+++ main.py
@@ -1,85 +1,71 @@
 import pyautogui
 import time 
-import warnings
 import os
 from pathlib import Path
 
-debug = True
+IMG_DIR = f'{os.getcwd()}/img/'
 
-BUILDING_FILENAMES = ['test_building']
-BUFF_IDS = ['fish_bowl', 'irma_basket', 'settler_add']
-IMG_DIR = Path(f'{os.getcwd()}/img/')
+BUFF_IDS = ['irma_basket']
+UI_COORDS = {'expand_settings_btn': (32, 467),
+               'camera_settings_btn': (106, 475),
+               'star_slider_default': (1902,767),
+               'star_default': (1596,670)}
 
-def trollFunction():
-    troll_string = 'twoja stara'
-    window_coords = {'firefox_input_bar' : [902,57]}
-    pyautogui.moveTo(window_coords['firefox_input_bar'][0],window_coords['firefox_input_bar'][1])
-    pyautogui.click()
-    pyautogui.write(troll_string, interval=0.25)
 
 class SettlerBot:
+
     class Debug:
+
         @staticmethod
         def getCursorPos(duration):
             while duration>0:
-                print(f'cursor pos={pyautogui.position()}')
+                print(f'cursor position={pyautogui.position()}')
                 time.sleep(1)
                 duration-=1
-
+        
     @staticmethod
-    def locateStarWindow():
+    def imgIntegrityCheck():
+        for i in BUFF_IDS:
+            file_path = Path(f'{IMG_DIR}+{i}.png')
+            if file_path.is_file():
+                print(f'image file for "{i}" exists.')
+            else: 
+                print(f'! image file for "{i}" doesnt exist !')
+
+    #Locates star menu window and drags it to a convenient position (bottom right corner).
+    @staticmethod
+    def starWindowLocate():
+        # max zoom out
+        pyautogui.scroll(-10)
         try:
+            # Dragging must be done twice, as TSO window dragging response is delayed.
             for i in range(2):
-                star_win = pyautogui.locateOnScreen('star_menu.png', grayscale=False, confidence=0.75)
-                pyautogui.moveTo(star_win.left+60, star_win.top+120)
-                print(f'star menu window located at x={star_win.left}, y={star_win.top}.')
-                pyautogui.drag(895-star_win.left,373-star_win.top, duration=2)
+                star_window = pyautogui.locateOnScreen(IMG_DIR+'star_menu.png', grayscale=False, confidence=0.6)
+                pyautogui.moveTo(star_window.left+40, star_window.top+150)
+                pyautogui.dragTo(UI_COORDS['star_default'][0], UI_COORDS['star_default'][1], duration=2)
         except pyautogui.ImageNotFoundException:
             print('star menu window not found.')
 
     @staticmethod
-    def imgIntegrityCheck():
-        for i in BUFF_IDS:
-            file_path = Path(f'{IMG_DIR}{i}.png')
-            if file_path.is_file():
-                print(f'plik dla "{i}" istnieje.')
-            else: 
-                print(f'! coś się zjebało. plik dla "{i}" nie istnieje. !')
-
-    @staticmethod
-    def starObjectLocate(buff_image):
-        buff_image = f'img/{buff_image}.png'
+    def starBuffSelect(buff_img):
+        buff_img = IMG_DIR+f'{buff_img}.png'
         while True:
             try:
-            #dostosuj parametr confidence dla poszcz. premii, aby nie mylił z innymi premiami
-                target_image = pyautogui.locateOnScreen(buff_image, confidence=0.75)
-                pyautogui.moveTo(target_image.left+20, target_image.top+20)
+            # + Add exclusive `confidence` parameter value for each image in the future to avoid recognition errors
+                target_img = pyautogui.locateOnScreen(buff_img, confidence=0.8)
+                pyautogui.moveTo(target_img.left+20, target_img.top+20)
             except pyautogui.ImageNotFoundException:
-                pyautogui.moveTo(1214,586)
-                pyautogui.drag(0,60, duration=2)
+                pyautogui.moveTo(UI_COORDS['star_slider_default'][0], UI_COORDS['star_slider_default'][1])
+                pyautogui.drag(0,70, duration=2)
                 continue
             break
 
     @staticmethod
-    # do poprawki (trzeba poprawić system rozszerzeń plików)
-    def starObjectApply(buff_image, building_image):
-        building_image = f'img/{building_image}.png'
+    # Create building map system utilizing fixed coordinates stored in json files.
+    def starBuffApply(buff_img):
+        SettlerBot.starBuffSelect(buff_img)
+        pyautogui.click(clicks=2, interval=0.75)
 
-        SettlerBot.starObjectLocate(buff_image)
 
-        pyautogui.click(clicks=2, interval=1)
-        try:
-            target_building = pyautogui.locateOnScreen(building_image, grayscale=False ,confidence=0.5)
-            pyautogui.moveTo(target_building.left+100, target_building.top+100)
-            print('building found.')
-        except pyautogui.ImageNotFoundException:
-            print('building not found')
-                
-
-# 4 zoomy w tył
-# 1214, 589
-
-SettlerBot.locateStarWindow()
-SettlerBot.starObjectApply('irma_basket', 'test_building')
-# SettlerBot.Debug.getCursorPos(10)
-
+if __name__ == '__main__':
+    SettlerBot.starWindowLocate()
